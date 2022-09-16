59601cd6f9a2598b208578a5f1b0d78d2a755635
BillyRonico <billyronico412@gmail.com>
2022-09-16 13:55:25 +0200

    fix exosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       repo: 'git@github.com:BillyRonico412/sudoku-solver-back.git',
       path: '/home/debian/pm2',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js',
       'pre-setup': ''
     }
   }
